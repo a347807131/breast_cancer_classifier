e0cf4916f94226fea0c3bb0b8d9642259bccf446
Gatsby <civin@bupt.cn>
2024-08-09 12:58:41 +0800

    更新修复breast_cancer_4接口

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import json
 import logging
 import os
+import pickle
 import subprocess
 import tempfile
 import uuid
@@ -79,26 +80,41 @@ def predict_breast_cancer(view: str = Form(...), image_file: UploadFile = File(.
 
 @app.post("/api/predict/breast_cancer_4")
 def predict_breast_cancer4(
-        image_files: List[UploadFile] = File(),
-        with_heatmap: bool = Form(default=False),
-    ):
-    logger.info(f"Received image file to do breast cancer 4. : {image_files}")
+    lcc: UploadFile = File(...),
+    rcc: UploadFile = File(...),
+    lmlo: UploadFile = File(...),
+    rmlo: UploadFile = File(...),
+    with_heatmap: bool = Form(default=False),
+):
+    logger.info("processing breast cancer 4")
     #写入文件
     temp_dir = tempfile.TemporaryDirectory()
-    for image_file in image_files:
-        with open(f"{temp_dir.name}/{image_file.filename}", "wb") as local_file:
-            local_file.write(image_file.file.read())
+    with open(f"{temp_dir.name}/0_L_CC.png", "wb") as local_file:
+        local_file.write(lcc.file.read())
+    with open(f"{temp_dir.name}/0_R_CC.png", "wb") as local_file:
+        local_file.write(rcc.file.read())
+    with open(f"{temp_dir.name}/0_L_MLO.png", "wb") as local_file:
+        local_file.write(lmlo.file.read())
+    with open(f"{temp_dir.name}/0_R_MLO.png", "wb") as local_file:
+        local_file.write(rmlo.file.read())
+
     out_dir_path = temp_dir.name
     out_file_path = f"{out_dir_path}/out.csv"
-    run_bash_script(
+    bash_result, err_out = run_bash_script(
         "run_without_heatmap.sh" if not with_heatmap else "run.sh",
         out_dir_path,  #images
         out_file_path
     )
+    logger.info(bash_result)
+    if(not os.path.exists(out_file_path)):
+        logger.error(err_out)
+        JSONResponse(content=ResponseModel(message=f"failed:{err_out}").dict())
     with open(out_file_path, encoding='utf-8') as csv_file:
-        out_string = csv_file.read()
+        reader=csv.DictReader(csv_file)
+        for row in reader:
+            data=row
     temp_dir.cleanup()
-    return JSONResponse(content=ResponseModel(message="Success", data=out_string).dict())
+    return JSONResponse(content=ResponseModel(message="Success", data=data).dict())
 
 
 if __name__ == '__main__':
--- run_without_heatmap.sh
+++ run_without_heatmap.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 NUM_PROCESSES=10
-DEVICE_TYPE='cpu'
+DEVICE_TYPE="cpu"
 NUM_EPOCHS=10
 HEATMAP_BATCH_SIZE=100
 GPU_NUMBER=0
 
 DATA_FOLDER=$1
-INITIAL_EXAM_LIST_PATH='sample_data/exam_list_before_cropping.pkl'
-PATCH_MODEL_PATH='models/sample_patch_model.p'
-IMAGE_MODEL_PATH='models/sample_image_model.p'
-IMAGEHEATMAPS_MODEL_PATH='models/sample_imageheatmaps_model.p'
+INITIAL_EXAM_LIST_PATH="sample_data/single_exam_before_cropping.pkl"
+PATCH_MODEL_PATH="models/sample_patch_model.p"
+IMAGE_MODEL_PATH="models/sample_image_model.p"
+IMAGEHEATMAPS_MODEL_PATH="models/sample_imageheatmaps_model.p"
 
-CROPPED_IMAGE_PATH='${DATA_FOLDER}/sample_output/cropped_images'
-CROPPED_EXAM_LIST_PATH='${DATA_FOLDER}/sample_output/cropped_images/cropped_exam_list.pkl'
-EXAM_LIST_PATH='${DATA_FOLDER}/sample_output/data.pkl'
-HEATMAPS_PATH='${DATA_FOLDER}/sample_output/heatmaps'
+CROPPED_IMAGE_PATH="${DATA_FOLDER}/cropped_images"
+CROPPED_EXAM_LIST_PATH="${DATA_FOLDER}/cropped_images/cropped_exam_list.pkl"
+EXAM_LIST_PATH="${DATA_FOLDER}/data.pkl"
+HEATMAPS_PATH="${DATA_FOLDER}/heatmaps"
 IMAGE_PREDICTIONS_PATH=$2
-IMAGEHEATMAPS_PREDICTIONS_PATH='${DATA_FOLDER}/sample_output/imageheatmaps_predictions.csv'
+IMAGEHEATMAPS_PREDICTIONS_PATH="${DATA_FOLDER}/imageheatmaps_predictions.csv"
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
 if [ $ENABLE_GPU ]; then
-    DEVICE_TYPE='gpu'
+    DEVICE_TYPE="gpu"
 fi
 
-echo 'Stage 1: Crop Mammograms'
+echo "Stage 1: Crop Mammograms"
 python3 src/cropping/crop_mammogram.py \
     --input-data-folder $DATA_FOLDER \
     --output-data-folder $CROPPED_IMAGE_PATH \
@@ -32,14 +32,14 @@ python3 src/cropping/crop_mammogram.py \
     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH  \
     --num-processes $NUM_PROCESSES
 
-echo 'Stage 2: Extract Centers'
+echo "Stage 2: Extract Centers"
 python3 src/optimal_centers/get_optimal_centers.py \
     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
     --data-prefix $CROPPED_IMAGE_PATH \
     --output-exam-list-path $EXAM_LIST_PATH \
     --num-processes $NUM_PROCESSES
 
-#echo 'Stage 3: Generate Heatmaps'
+#echo "Stage 3: Generate Heatmaps"
 #python3 src/heatmaps/run_producer.py \
 #    --model-path $PATCH_MODEL_PATH \
 #    --data-path $EXAM_LIST_PATH \
@@ -49,7 +49,7 @@ python3 src/optimal_centers/get_optimal_centers.py \
 #    --device-type $DEVICE_TYPE \
 #    --gpu-number $GPU_NUMBER
 
-echo 'Stage 4a: Run Classifier (Image)'
+echo "Stage 4a: Run Classifier (Image)"
 python3 src/modeling/run_model.py \
     --model-path $IMAGE_MODEL_PATH \
     --data-path $EXAM_LIST_PATH \
@@ -60,7 +60,7 @@ python3 src/modeling/run_model.py \
     --device-type $DEVICE_TYPE \
     --gpu-number $GPU_NUMBER
 
-#echo 'Stage 4b: Run Classifier (Image+Heatmaps)'
+#echo "Stage 4b: Run Classifier (Image+Heatmaps)"
 #python3 src/modeling/run_model.py \
 #    --model-path $IMAGEHEATMAPS_MODEL_PATH \
 #    --data-path $EXAM_LIST_PATH \
